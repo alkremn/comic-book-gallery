8a40245eb85eab70e685ba7129f3e038ea2c69e0
alkremn <alkremn@gmail.com>
2018-12-04 20:30:29 -0700

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,15 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-
-            }
-            else
-            {
-                return Content("Hello there fella!");
-            }
+            return View();
         }
     }
 
